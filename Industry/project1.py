7f815e031ee53cd3cdab9b38bcff6c081ac69467
Your Name <you@example.com>
2021-10-22 09:58:03 +0530

    .

--- Industry/project1.py
+++ Industry/project1.py
@@ -1,14 +1,15 @@
-import cv2
 import pytesseract as pytesseract
+import re
 from PIL import Image
-
-def ocr(img):
-    text=pytesseract.image_to_string(img)
-    return text
-img=cv2.imread('imvoice1.png')
-def grey(image):
-    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-img=grey(img)
-print(ocr(img))
-
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+text=pytesseract.image_to_string(Image.open(r'invoice6.png'))
+# print(text)
+num1 = re.findall(r'\d+\.\d+', text)
+print(num1)
+    # num.sort()
+list1 = sorted(num1, key=float)
+a=list1[len(list1)-1]
+print(a)
+# num2 = re.findall(r'\d+\,\d+\.\d+', text)
+# if total==num1 or total==num2:
+# or num=re.findall(r'\d+\.\d+\,\d+', text)
