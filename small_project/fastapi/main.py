a99735502380aa0fd313a54f3c0af4704f7e670b
dhruv-suraj <dhruv.suraj@somiaya.edu>
2021-11-01 14:06:29 +0530

    .

--- small_project/fastapi/main.py
+++ small_project/fastapi/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI,Path
 from typing import  Optional
 app = FastAPI()
-
-class Item():
-    name:str
-    price:float
-    brand:Optional[str]=None
-
 # @app.get("/")
 # def read_root():
 #     return {"Hello": "World"}
@@ -14,9 +8,13 @@ class Item():
 # @app.get("/about")
 # def about():
 #     return {"About":"Dhruv"}
-
-inventory = {}
-
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "amul"
+    }
+}
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int):
     return inventory[item_id]
@@ -27,11 +25,4 @@ def get_item(test: int,name: Optional[str]=None):
         return inventory[item_id]
     return {"Data": "Not found"}
 # #http://127.0.0.1:8000/get-by-name?name=Milk
-
-@app.post("/create-item/{item_id}")
-def create_item(item_id:int,item:Item):
-    if item_id in inventory:
-        return{"Error":"Item Id already exists"}
-        
-    inventory[item_id]=item
-    return inventory[item_id]
+#
